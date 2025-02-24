b8729c54cfaad488d5f33c075b52c0dfb9c6123b
badr <badryounes@hotmail.be>
2025-02-24 17:04:38 +0000

    aanpassingen gedaan

--- js/popup.js
+++ js/popup.js
@@ -1,41 +1,40 @@
-// Get the popup
+
 let popup = document.getElementById("popup");
 let closePopup = document.getElementById("close-popup");
 
-// Function to show the popup
+
 function showPopup() {
-    popup.classList.add("active"); // Show the popup by adding the active class
+    popup.classList.add("active"); 
 }
 
-// Function to hide the popup
+
 function hidePopup() {
-    popup.classList.remove("active"); // Hide the popup by removing the active class
+    popup.classList.remove("active"); 
 }
 
-// Add event listeners to the slider items
+
 let sliderItems = document.querySelectorAll('.wrong-slider a');
 
 sliderItems.forEach(function(item) {
     item.addEventListener('click', function(event) {
        let  title = item.querySelector('h2').innerText;
         
-        // Check if the clicked item is "Magic The Gathering"
+       
         if (title === "Magic The Gathering") {
-            return; // Do nothing if the item is "Magic The Gathering"
+            return; 
         }
 
-        // For other items, show the popup
-        event.preventDefault(); // Prevent the default link behavior
-        showPopup(); // Show the popup
+        event.preventDefault(); 
+        showPopup(); 
     });
 });
 
-// When the user clicks on <span> (x), close the popup
+// alsje op de  (x) drukt, sluit de popup
 closePopup.onclick = function() {
     hidePopup();
 }
 
-// When the user clicks anywhere outside of the popup, close it
+// buiten de popup klikken sluit de pop up
 window.onclick = function(event) {
     if (event.target == popup) {
         hidePopup();
